4069b72070c3d19d8cbc44fad3907f7a4327800e
Konrad McClure <konradmcclure@gmail.com>
2021-05-25 16:13:15 -0700

    Updated some files as troubleshooting happened last week. Forgot to commit

--- pwmtest/pwm.py
+++ pwmtest/pwm.py
@@ -7,12 +7,12 @@ stirrer = pwmio.PWMOut(board.D13, duty_cycle=0,frequency=100)
 
 while True:
 	input("Press enter to start up")
-	print("Starting Up Slow Mode")
-	stirrer.duty_cycle = 30000
-	print("Stirrer set to ", stirrer.duty_cycle)
-	time.sleep(2.0)
-	stirrer.duty_cycle = 10000
-	print("Stirrer set to ", stirrer.duty_cycle)
+	# print("Starting Up Slow Mode")
+	# stirrer.duty_cycle = 30000
+	# print("Stirrer set to ", stirrer.duty_cycle)
+	# time.sleep(2.0)
+	# stirrer.duty_cycle = 10000
+	# print("Stirrer set to ", stirrer.duty_cycle)
 	
 	selection = -1
 	while (selection != 3):
