c5021677e64ad826b18da56229d7f7cc8af7750d
Konrad <konradmcclure@gmail.com>
2021-08-18 17:18:48 -0700

    Changed pwm values for new small magnets

--- pwmtest/pwm.py
+++ pwmtest/pwm.py
@@ -7,20 +7,20 @@ stirrer = pwmio.PWMOut(board.D13, duty_cycle=0,frequency=100)
 
 while True:
 	input("Press enter to start up")
-	# print("Starting Up Slow Mode")
-	# stirrer.duty_cycle = 30000
-	# print("Stirrer set to ", stirrer.duty_cycle)
-	# time.sleep(2.0)
-	# stirrer.duty_cycle = 10000
-	# print("Stirrer set to ", stirrer.duty_cycle)
+	print("Starting Up Slow Mode")
+	stirrer.duty_cycle = 7500
+	print("Stirrer set to ", stirrer.duty_cycle)
+	time.sleep(0.5)
+	stirrer.duty_cycle = 9000
+	print("Stirrer set to ", stirrer.duty_cycle)
 	
 	selection = -1
 	while (selection != 3):
 		selection = int(input("Select mode:\n\tSlow: 1\n\tFast: 2\n\tStop: 3\n\tCustom: 4\n>"))
 		if (selection == 1):
-			stirrer.duty_cycle = 10000
+			stirrer.duty_cycle = 9000
 		elif selection == 2:
-			stirrer.duty_cycle = 15000
+			stirrer.duty_cycle = 12000
 		elif selection == 3:
 			stirrer.duty_cycle = 0
 		elif selection == 4:
