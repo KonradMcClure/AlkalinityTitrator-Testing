08010ce036e02aa834811c7550f14650d9fe846a
Konrad <konradmcclure@gmail.com>
2021-08-18 17:20:37 -0700

    With the small magenets, we don't even need the kick startup.

--- pwmtest/pwm.py
+++ pwmtest/pwm.py
@@ -8,9 +8,6 @@ stirrer = pwmio.PWMOut(board.D13, duty_cycle=0,frequency=100)
 while True:
 	input("Press enter to start up")
 	print("Starting Up Slow Mode")
-	stirrer.duty_cycle = 7500
-	print("Stirrer set to ", stirrer.duty_cycle)
-	time.sleep(0.5)
 	stirrer.duty_cycle = 9000
 	print("Stirrer set to ", stirrer.duty_cycle)
 	
