e97e04eed6ebfb6d2a65ffb97c015c9b67e698c0
Konrad <konradmcclure@gmail.com>
2021-08-18 22:31:51 -0700

    pwm test update, lower values for new magnet holder

--- pwmtest/pwm.py
+++ pwmtest/pwm.py
@@ -6,26 +6,26 @@ import math
 def change_pwm(stirrer, target):
 	direction = math.copysign(1, target - stirrer.duty_cycle)
 	while stirrer.duty_cycle != target:
-		next_step = min(abs(target - stirrer.duty_cycle), 1000)
+		next_step = min(abs(target - stirrer.duty_cycle), 100)
 		stirrer.duty_cycle = stirrer.duty_cycle + (next_step * direction)
 		print("Stirrer set to ", stirrer.duty_cycle)
-		time.sleep(0.2)
+		time.sleep(0.1)
 
 stirrer = pwmio.PWMOut(board.D13, duty_cycle=0,frequency=100)
 
 while True:
 	input("Press enter to start up")
 	print("Starting Up Slow Mode")
-	stirrer.duty_cycle = 9000
+	stirrer.duty_cycle = 3000
 	print("Stirrer set to ", stirrer.duty_cycle)
 	
 	selection = -1
 	while (selection != 3):
 		selection = int(input("Select mode:\n\tSlow: 1\n\tFast: 2\n\tStop: 3\n\tCustom: 4\n>"))
 		if (selection == 1):
-			change_pwm(stirrer, 9000)
+			change_pwm(stirrer, 3000)
 		elif selection == 2:
-			change_pwm(stirrer, 12000)
+			change_pwm(stirrer, 5000)
 		elif selection == 3:
 			stirrer.duty_cycle = 0
 			print("Stirrer set to ", stirrer.duty_cycle)
